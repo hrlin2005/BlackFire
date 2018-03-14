9cecb289b7ca9ea2223d7ac3d9b2388a8a14c98f
BlackFire <36105419+hrlin2005@users.noreply.github.com>
2018-03-14 11:47:02 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -529,7 +529,7 @@ Debian_apt(){
 Download_SSR(){
 	cd "/usr/local"
 	#git config --global http.sslVerify false
-	env GIT_SSL_NO_VERIFY=true git clone -b manyuser https://github.com/hrlin2005/BlackFire.git
+	env GIT_SSL_NO_VERIFY=true git clone -b master https://github.com/hrlin2005/BlackFire.git
 	[[ ! -e ${ssr_folder} ]] && echo -e "${Error} ShadowsocksR服务端 下载失败 !" && exit 1
 	[[ -e ${config_folder} ]] && rm -rf ${config_folder}
 	mkdir ${config_folder}
